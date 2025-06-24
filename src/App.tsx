44a7c8878133b2df049d107cccbfbba8e60a4c0e
iamhabbeboy <iamhabbeboy@gmail.com>
2025-06-24 21:49:20 +0100

    chore: implement product listing ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import type { IPost } from './types/IPost'
-import axios from "axios"
-import Post from './components/Posts';
+import Products from './components/Products';
+import "./App.css"
 
 function App() {
-  const [posts, setPosts] = useState<IPost[]>([]);
-  const fetchPosts = async () => {
-    try {
-      const response = await axios.get<IPost[]>("https://jsonplaceholder.typicode.com/posts?_limit=10");
-      setPosts(response.data)
-    } catch(e) {
-      console.error(e)
-    }
-  }
-  useEffect(() => {
-    if(!posts.length) {
-    fetchPosts()
-    }
-  }, [posts])
+  // const [posts, setPosts] = useState<IPost[]>([]);
+  // const fetchPosts = async () => {
+  //   try {
+  //     const response = await axios.get<IPost[]>("https://jsonplaceholder.typicode.com/posts?_limit=10");
+  //     setPosts(response.data)
+  //   } catch(e) {
+  //     console.error(e)
+  //   }
+  // }
+  // useEffect(() => {
+  //   if(!posts.length) {
+  //   fetchPosts()
+  //   }
+  // }, [posts])
   return (
-      <div className='mx-auto mt-10 bg-gray-100 rounded-md container p-10'> 
-        <h1 className='text-indigo-400 text-2xl'> Posts </h1>
+      <div className='mx-auto mt-10 rounded-md container p-10'> 
+      <Products />
+        {/* <h1 className='text-indigo-400 text-2xl'> Posts </h1>
         <div className='mt-5'>
           {posts.map((post, key) => (
           <Post {...post} key={key} />
           ))}
-        </div>
+        </div> */}
       </div>
   )
 }
---
+++ src/components/Product.tsx
@@ -0,0 +1,17 @@
+import type { IProduct } from "../types/IProduct"
+
+const Product: React.FC<IProduct> = ({title, image, price, oldPrice, discount}) => {
+    return (
+        <div>
+            <div className={`bg-gray-300 w-[300px] h-[400px] rounded-md mr-3`} style={{backgroundImage: `url('${image}')`, backgroundSize: "cover"}}>
+            </div>
+            <h2 className="mt-5">{title}</h2>
+            <div className="flex justify-between w-9/12">
+             <span className="line-through font-bold">{oldPrice}</span>
+             <span className="font-bold">{price}</span>
+             <span className="bg-black p-2 text-white text-sm">{discount}</span>
+            </div>
+        </div>
+    )
+}
+export default Product
